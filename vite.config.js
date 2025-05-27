b043fcfaa87ce536b200d9f17169f7066d77f5ff
mohit <mohitkumarsahu0110@gmail.com>
2025-05-28 00:01:12 +0530

    made changes to deploy error-free on vercel.

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,8 @@ import tagger from "@dhiwise/component-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tagger()],
-  build: {
-    outDir: "build",
+  plugins: [react(), tagger()], build: {
+    outDir: "dist",
   },
   resolve: {
     alias: {
